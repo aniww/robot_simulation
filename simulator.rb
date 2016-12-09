d2a7efbe1a2812703549e5c5108c96ef1ba3eeb9
aniww <anant.kolvankar@wwindia.com>
2016-12-09 16:24:09 +0530

    Added argument check.

--- simulator.rb
+++ simulator.rb
@@ -1,5 +1,9 @@
 require './application'
 # Set max dimension values for table grid below example is for 5 X 5 units.
-dimension_x = 5
-dimension_y = 5
-app = Application.new(ARGV[0], dimension_x, dimension_y)
+if ARGV[0]
+  dimension_x = 5
+  dimension_y = 5
+  app = Application.new(ARGV[0], dimension_x, dimension_y)
+else
+  puts "File path not provided \nEx:- ruby simulator.rb test_data.txt"
+end
